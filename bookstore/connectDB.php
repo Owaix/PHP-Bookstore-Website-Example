46b368051310d37502b08991c7f888e648161c9d
Owaix <aowais37@gmail.com>
2022-10-25 19:04:36 +0500

    Update connectDB.php

--- bookstore/connectDB.php
+++ bookstore/connectDB.php
@@ -1,3 +1,3 @@
 <?php
-$pdo=new PDO('mysql:host=localhost;port=3306;dbname=bookstore','root', '');
+$pdo=new PDO('mysql:host=ls-d1b6e3de08e34395cddc4f6774238bd258ceb4eb.c6uay4qn3ulx.ap-southeast-1.rds.amazonaws.com;port=3306;dbname=bookstore','owaix', 'Ziawospmahc1');
 ?>
